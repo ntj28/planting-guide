958cea73ed4489ba327c0ccbedabeaea4274980d
ntj28 <nelsonTejara@yahoo.com>
2016-11-19 07:11:51 +0000

    modified the  forecast ten day file

--- server/api/wunderground_forecast_ten.js
+++ server/api/wunderground_forecast_ten.js
@@ -2,9 +2,9 @@ Meteor.methods ({
 	'getDataWundergroundForecast' : function (province,city,awsID) {
     //http://api.wunderground.com/api//history_20161115/q/pws:ICALABAR18.json
     
-     securityKey = ""
+     securityKey = "60d1fb8794e7bb72"
     // Construct the API URL http://api.wunderground.com/api/60d1fb8794e7bb72/forecast10day/q/Ilocos_Norte/Batac.json
-    const apiUrl = 'http://api.wunderground.com/api/securityKey/forecast10day/q/pws:' + awsID +'.json';
+    const apiUrl = 'http://api.wunderground.com/api/60d1fb8794e7bb72/forecast10day/q/pws:' + awsID +'.json';
     //const apiUrl = 'http://api.wunderground.com/api/60d1fb8794e7bb72/forecast10day/q/' + province + '/' + city + '.json';
     console.log('trial', apiUrl);
     // query the API

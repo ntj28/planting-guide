7d216bd72cea54f8e0f37a2b9dbc7b272f50e375
ntj28 <nelsonTejara@yahoo.com>
2016-11-19 12:44:06 +0000

    added the  /api for getting the wunderground history as well as getting the ten days forecast

---
+++ server/api/wunderground_forecast_ten.js
@@ -0,0 +1,19 @@
+Meteor.methods ({
+	'getDataWundergroundForecast' : function (province,city,awsID) {
+    //http://api.wunderground.com/api//history_20161115/q/pws:ICALABAR18.json
+    
+     securityKey = ""
+    // Construct the API URL http://api.wunderground.com/api/60d1fb8794e7bb72/forecast10day/q/Ilocos_Norte/Batac.json
+    const apiUrl = 'http://api.wunderground.com/api/securityKey/forecast10day/q/pws:' + awsID +'.json';
+    //const apiUrl = 'http://api.wunderground.com/api/60d1fb8794e7bb72/forecast10day/q/' + province + '/' + city + '.json';
+    console.log('trial', apiUrl);
+    // query the API
+    var response = HTTP.get(apiUrl);
+    return response;
+    console.log(apiUrl)
+
+	 
+	},
+
+	 
+})

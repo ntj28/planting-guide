486692bf6944ecbc9293cd5b491681589e6684fd
ntj28 <nelsonTejara@yahoo.com>
2016-11-19 12:47:39 +0000

    added crops.js in the file to handle methods involving crops like insert, update and delete

---
+++ server/methods/crops.js
@@ -0,0 +1,31 @@
+import { cropsCollection } from '../../lib/collections/crops.js'
+
+Meteor.methods ({
+	'add-crop' : function (crop) {
+		cropsCollection.insert({
+			crop : crop			 
+		})
+	},
+
+	'delete-crop': function(id){
+        cropsCollection.remove({
+            _id:id
+        })
+    },
+
+    'update-crop' : function(_id,crop) {
+        cropsCollection.update (
+            {_id},//filters to be updaated
+            {
+                $set: {
+                    crop:crop,
+                                        
+                }
+            },
+
+                {upsert : true}
+            
+        )
+
+    },
+})

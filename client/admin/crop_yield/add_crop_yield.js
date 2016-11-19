4adcbe21eba8f115ff48550a3d2b299654705adb
ntj28 <nelsonTejara@yahoo.com>
2016-11-19 12:06:33 +0000

    modified add_crop_yield.js

--- client/admin/crop_yield/add_crop_yield.js
+++ client/admin/crop_yield/add_crop_yield.js
@@ -1,19 +1,30 @@
+import { cropsCollection } from '../../../lib/collections/crops.js'
+
+Template.AddCropYield.helpers ({
+    cropData: function() {
+        const cropData  = cropsCollection.find({}).fetch()         
+        return cropData
+    }    
+})
+
+
+
 Template.AddCropYield.events({
 	'click #AddCropYieldButton' : function (e) {
 		//getting the  name fields and thier values
 		const locationID = FlowRouter.getParam('location_id')
 		console.log(locationID)        
-        const cropTypeField = $('#cropType')
-        const cropType = cropTypeField.val()
+        const cropField = $('#cropSelection')
+        const cropType = cropField.val()
         const cropYieldField = $('#cropYield')
         const cropYield = cropYieldField.val()
+        
          
         //saving the  data
 		Meteor.call ('add-crop-yield',locationID,cropType,cropYield)
 		console.log('added')
 		FlowRouter.go(`/crop_yield/${locationID}`)
 		//clearing the  fields
-		cropTypeField.val = ' '
 		cropYieldField.val =' '
 	},
 

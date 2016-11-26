f106bb4a4f589adcb4b29e09ed28f392ba7444d7
ntj28 <nelsonTejara@yahoo.com>
2016-11-26 05:25:12 +0000

    allows editing of crop type

--- client/admin/duration_yield/edit_duration_yield.js
+++ client/admin/duration_yield/edit_duration_yield.js
@@ -1,39 +1,39 @@
 import { durationYields } from '../../../lib/collections/duration_yield.js'
+import { cropsCollection } from '../../../lib/collections/crops.js'
 
 Template.EditDurationYield.helpers({
 	data:()=> {
 		const _id = FlowRouter.getParam('yield_id')
 		const data = durationYields.findOne({_id})
 		return data
-	}
+	},
+
+        cropData: function() {
+                const cropData  = cropsCollection.find({}).fetch()         
+                return cropData
+        }  
 })
 
 
 Template.EditDurationYield.events ({
 	'click #update-duration-yield-button ' : function(e) {
+
 		//getting the  fields as well as the data
-        const locationID = FlowRouter.getParam('location_id')
-	const durationYieldID = FlowRouter.getParam('yield_id')
-        const locationField = $("#location")
-        const location = locationField.val()
-        const weekNoField = $("#weekNo")
-        const weekNo = weekNoField.val()
-        const startDateField = $("#startDate")
-        const startDate = startDateField.val()
-        const endDateField = $("#endDate")
-        const endDate = endDateField.val()
-        const yieldField = $("#yield")
-        const yields = yieldField.val()
+                const locationID = FlowRouter.getParam('location_id')
+        	const durationYieldID = FlowRouter.getParam('yield_id')                
+                const weekNoField = $("#weekNo")
+                const weekNo = weekNoField.val()               
+                const yieldField = $("#yield")
+                const yields = yieldField.val()
+                const cropField = $('#cropSelection')
+                const cropType = cropField.val()
         
 
-        Meteor.call('update-duration-yield',durationYieldID,locationID,weekNo,startDate,endDate,yields)
-                
+        Meteor.call('update-duration-yield',durationYieldID,locationID,cropType,weekNo,yields)           
         
-        console.log("added")
-        locationField.val = " "
-        weekNoField.val = " "
-        startDateField.val = " "
-        endDateField.val = " "
+ 
+        
+        weekNoField.val = " "        
         yieldField.val = " "
 
         FlowRouter.go(`/duration_yield/${locationID}`)

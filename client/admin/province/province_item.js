15f578839ac61aaf736bef1f17f42ffb9ead0a20
ntj28 <nelsonTejara@yahoo.com>
2016-11-26 23:59:42 +0000

    redirects user to login page if not logged in

--- client/admin/province/province_item.js
+++ client/admin/province/province_item.js
@@ -1,3 +1,15 @@
+Template.ProvinceItem.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});
+
 Template.ProvinceItem.events({
 	'click #edit-province-button' : function(e) {
 		FlowRouter.go(`/editProvince/${this._id}`)

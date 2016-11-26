28e2f19c6faa148125dc9b98b6aed1a42c2faa94
ntj28 <nelsonTejara@yahoo.com>
2016-11-26 23:42:06 +0000

    redirects user to login page if not logged in

--- client/admin/amount_rainfall/amount_rainfall-item.js
+++ client/admin/amount_rainfall/amount_rainfall-item.js
@@ -1,3 +1,15 @@
+Template.RainfallItem.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});
+
 Template.RainfallItem.events({
 	'click #edit-rainfall-button' : function (e) {
 		//const id = FlowRouter.getParam('location_id')

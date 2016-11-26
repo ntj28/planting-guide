47c543f951833005927342a02a83e6e92fdce99d
ntj28 <nelsonTejara@yahoo.com>
2016-11-26 23:47:41 +0000

    redirects user to login page if not logged in

--- client/admin/city/city.js
+++ client/admin/city/city.js
@@ -1,5 +1,18 @@
 import { cityCollection  } from '../../../lib/collections/city.js'
 
+Template.City.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});
+
+
 Template.City.helpers ({
 	city: function() {
 		const _id = FlowRouter.getParam('province_id')

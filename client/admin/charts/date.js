135e948d44f39417002b5238e848302169d74d2e
ntj28 <nelsonTejara@yahoo.com>
2016-11-26 23:45:58 +0000

    redirects user to login page if not logged in

--- client/admin/charts/date.js
+++ client/admin/charts/date.js
@@ -1,3 +1,15 @@
+Template.EnterDate.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});
+
 Template.EnterDate.events({
 	'click #date-button ' : function (e) {
 		const awsID = FlowRouter.getParam('awsID')

0430868f5c28b596ac0cc9c658516a59e1e91ce5
ntj28 <nelsonTejara@yahoo.com>
2016-11-26 23:49:31 +0000

    redirects user to login page if not logged in

--- client/admin/crop_yield/add_crop_yield.js
+++ client/admin/crop_yield/add_crop_yield.js
@@ -1,5 +1,18 @@
 import { cropsCollection } from '../../../lib/collections/crops.js'
 
+Template.AddCropYield.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});
+
+
 Template.AddCropYield.helpers ({
     cropData: function() {
         const cropData  = cropsCollection.find({}).fetch()         

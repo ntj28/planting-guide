b13b34d5e1eead6e3b161242e7519108dca198c6
ntj28 <nelsonTejara@yahoo.com>
2016-11-26 23:59:05 +0000

    redirects user if not logged in

--- client/admin/province/province.js
+++ client/admin/province/province.js
@@ -1,5 +1,18 @@
 import { province  } from '../../../lib/collections/province.js'
 
+Template.Province.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});
+
+
 Template.Province.helpers ({
 	provinces: function() {
 		const provinces = province.find({}).fetch()

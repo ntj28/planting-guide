e914cdf50f0016b96fdd4ef594ab6a60dfe2d9e7
ntj28 <nelsonTejara@yahoo.com>
2016-11-27 00:00:56 +0000

    redirects user to login page if not logged in

--- client/admin/registration/navigation.js
+++ client/admin/registration/navigation.js
@@ -1,3 +1,15 @@
+Template.navigation.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});
+
 Template.navigation.events({
     'click .logout': function(event){
         event.preventDefault();

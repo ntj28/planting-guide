dea31fd7e3b914f7a2aabdc4b7f121dc1dd501e7
ntj28 <nelsonTejara@yahoo.com>
2016-11-27 00:08:14 +0000

    redirects user to login page if not logged in

---
+++ client/admin/crop_yield/crops.js
@@ -0,0 +1,11 @@
+Template.cropItems.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});

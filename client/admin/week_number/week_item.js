21e9bd0cd007c3603a136ccfbfc9cefe23eb7115
ntj28 <nelsonTejara@yahoo.com>
2016-11-27 00:06:32 +0000

    redirects user to login page if not logged in

--- client/admin/week_number/week_item.js
+++ client/admin/week_number/week_item.js
@@ -1,3 +1,16 @@
+Template.weekItem.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});
+
+
 Template.weekItem.events({
 	'click #edit-week-button' : function(e) {
 		FlowRouter.go(`/editWeek/${this._id}`)

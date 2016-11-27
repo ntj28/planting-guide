422eb8ef68353de7a7017823f93fe99c068c08f4
ntj28 <nelsonTejara@yahoo.com>
2016-11-27 00:07:36 +0000

    redirects user to login page if not logged in

--- client/admin/week_number/week_number.js
+++ client/admin/week_number/week_number.js
@@ -1,5 +1,17 @@
 import { weekNoCollection  } from '../../../lib/collections/week_no.js'
 
+Template.weekNumber.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});
+
 Template.weekNumber.helpers ({
 	weekData: function() {
 		const weekData = weekNoCollection.find({}).fetch()

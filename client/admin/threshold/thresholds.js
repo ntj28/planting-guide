e883b15987b2d93b244a4670b275b2b823a6ad8b
ntj28 <nelsonTejara@yahoo.com>
2016-11-27 00:03:55 +0000

    redirects user to login page if not logged in

--- client/admin/threshold/thresholds.js
+++ client/admin/threshold/thresholds.js
@@ -1,5 +1,18 @@
 import { thresholdsCollection  } from '../../../lib/collections/thresholds.js'
 
+
+Template.Thresholds.onCreated( () => {
+
+  var currentUser = Meteor.userId();
+        if(currentUser){
+            // logged-in
+        } else {
+            // not logged-in
+            FlowRouter.go('/')
+
+        }
+});
+
 Template.Thresholds.helpers ({
 	thresholdData: function() {
 		const thresholdData = thresholdsCollection.find({}).fetch()

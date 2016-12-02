bfd6c2b5acbd5f74427f27d4b3ab10b59b8935d7
ntj28 <nelsonTejara@yahoo.com>
2016-12-02 15:43:43 +0000

    added subscribed method

--- client/admin/crop_yield/crop_yield_index.js
+++ client/admin/crop_yield/crop_yield_index.js
@@ -5,6 +5,7 @@ Template.CropYield.onCreated( () => {
   var currentUser = Meteor.userId();
         if(currentUser){
             // logged-in
+            Meteor.subscribe('cropYields')
         } else {
             // not logged-in
             FlowRouter.go('/')

f20bb9308cc19938f9bc95a6aad81d44a377e6e0
ntj28 <nelsonTejara@yahoo.com>
2016-12-04 23:03:07 +0000

    populates the options tag from mongodb

--- client/admin/threshold/add_thresholds.js
+++ client/admin/threshold/add_thresholds.js
@@ -1,8 +1,13 @@
+import { cropsCollection } from '../../../lib/collections/crops.js'
+import { cropVarietiesCollection  } from '../../../lib/collections/crop_varieties.js'
+
 Template.AddThreshold.onCreated( () => {
 
   var currentUser = Meteor.userId();
         if(currentUser){
             // logged-in
+            Meteor.subscribe('crops')
+            Meteor.subscribe('cropVarieties')
         } else {
             // not logged-in
             FlowRouter.go('/')
@@ -10,19 +15,76 @@ Template.AddThreshold.onCreated( () => {
         }
 });
 
+Template.AddThreshold.helpers ({
+    dataCrop: function() {
+        const dataCrop  = cropsCollection.find({}).fetch()         
+        return dataCrop
+    },
+
+    dataVariety: function() {
+        const dataVariety  = cropVarietiesCollection.find({}).fetch()         
+        return dataVariety
+    }     
+})
+
+/*Meteor.autorun(() => {    
+    //populate the  cropType select
+    const cropData  = cropsCollection.find({}).fetch()
+    const cropField = $('#cropSelectionThresholdAdd')
+    cropField.empty()
+    cropData.forEach((item) => {        
+            cropField.append("<option value =" + item._id +">" + item.crop + "</option>");        
+    }); 
+
+    //populate the crop variety
+    const varietyField = $('#cropVarietySelectionThresholdAdd')
+    varietyField.empty()
+    const cropID =  cropField.val()
+    const dataVariety  = cropVarietiesCollection.find({cropID: cropID}).fetch()
+
+    //adds options to the select tag
+    
+    dataVariety.forEach((item) => {       
+            varietyField.append("<option value =" + item.variety +">" + item.variety + "</option>");     
+        
+    });
+
+    //  const cropVarietyData  = cropVarietiesCollection.find({}).fetch() 
+  //console.log("on rendered function"+cropVarietyData.length)
+    
+}); */
+
+
 Template.AddThreshold.events ({
+
+    'change #cropSelectionThresholdAdd' : function(e){
+        //get the value of  the  province field
+        const cropField = $('#cropSelectionThresholdAdd') 
+        const cropID =  cropField.val()    
+
+        //gets the variety field and empty its options
+        const varietyField = $('#cropVarietySelectionThresholdAdd')
+        varietyField.empty()
+        //retrieves the list of city as specified by what province is selected
+        const dataVariety  = cropVarietiesCollection.find({cropID: cropID}).fetch() 
+        //adds options to the select tag
+        dataVariety.forEach((item) => {
+            varietyField.append("<option>" + item.variety + "</option>");
+        });   
+
+    },
+
 	'click #add-threshold ' : function (e) {
 		
-		const cropField = $('#crop')
-        const crop = cropField.val()
+		const cropType = $('#cropSelectionThresholdAdd').find('option:selected').text()
+        const cropVariety = $('#cropVarietySelectionThresholdAdd').find('option:selected').text()
         const daysField = $('#days')
         const days = daysField.val()
         const amountRainfallField = $('#amountRainfall')
         const amountRainfall = amountRainfallField.val()
 		
-		Meteor.call ('add-thresholds',crop,days,amountRainfall)
-		console.log ("added")
-		cropField.val = " "
+		Meteor.call ('add-thresholds',cropType,cropVariety,days,amountRainfall)
+		console.log ("added")		
 		daysField.val = " "
 		amountRainfallField.val = " "
 

417e58eca319a622346f66fcde04939d541e075f
ntj28 <nelsonTejara@yahoo.com>
2016-12-04 22:55:06 +0000

    populated the select tag from mongodb

--- client/admin/crop_yield/add_crop_yield.js
+++ client/admin/crop_yield/add_crop_yield.js
@@ -15,29 +15,58 @@ Template.AddCropYield.onCreated( () => {
         }
 });
 
-
 Template.AddCropYield.helpers ({
-    cropData: function() {
-        const cropData  = cropsCollection.find({}).fetch()         
-        return cropData
+    dataCrop: function() {
+        const dataCrop  = cropsCollection.find({}).fetch()         
+        return dataCrop
     },
-    cropVarietyData: function() {
-        const cropVarietyData  = cropVarietiesCollection.find({}).fetch()         
-        return cropVarietyData
-    }   
+
+    dataVariety: function() {
+        const dataVariety  = cropVarietiesCollection.find({}).fetch()         
+        return dataVariety
+    }     
 })
+
+/*Meteor.autorun(() => {    
+    //populate the  cropType select
+    const cropData  = cropsCollection.find({}).fetch()
+    const cropField = $('#cropSelectionCropYieldAdd')
+    cropField.empty()
+    cropData.forEach((item) => {        
+            cropField.append("<option value =" + item._id +">" + item.crop + "</option>");        
+    }); 
+
+    //populate the crop variety
+    const varietyField = $('#cropVarietySelectionCropYieldAdd')
+    varietyField.empty()
+    const cropID =  cropField.val()
+    const dataVariety  = cropVarietiesCollection.find({cropID: cropID}).fetch()
+
+    //adds options to the select tag
+    
+    dataVariety.forEach((item) => { 
+
+            varietyField.append("<option value =" + item.variety +">" + item.variety + "</option>");     
+        
+    });
+    console.log("autorun heere")  
+
+    //  const cropVarietyData  = cropVarietiesCollection.find({}).fetch() 
+  //console.log("on rendered function"+cropVarietyData.length)
+    
+}); */
 
 
 
 Template.AddCropYield.events({
 
-    'change #cropSelection' : function(e){
+    'change #cropSelectionCropYieldAdd' : function(e){
         //get the value of  the  province field
-        const cropField = $('#cropSelection') 
+        const cropField = $('#cropSelectionCropYieldAdd') 
         const cropID =  cropField.val()    
 
         //gets the variety field and empty its options
-        const varietyField = $('#cropVarietySelection')
+        const varietyField = $('#cropVarietySelectionCropYieldAdd')
         varietyField.empty()
         //retrieves the list of city as specified by what province is selected
         const dataVariety  = cropVarietiesCollection.find({cropID: cropID}).fetch() 
@@ -54,11 +83,10 @@ Template.AddCropYield.events({
 		//getting the  name fields and thier values
 		const locationID = FlowRouter.getParam('location_id')
 		console.log(locationID)       
-        const cropType = $('#cropSelection').find('option:selected').text()
+        const cropType = $('#cropSelectionCropYieldAdd').find('option:selected').text()
         const cropYieldField = $('#cropYield')
-        const cropYield = cropYieldField.val()
-        const varietyField = $('#cropVarietySelection')
-        const cropVariety = varietyField.val()
+        const cropYield = cropYieldField.val()        
+        const cropVariety = $('#cropVarietySelectionCropYieldAdd').find('option:selected').text()
         
          
         //saving the  data
